466bc0aff45b407561690923cc515af5f985cdf6
rockinrobin714 <robindykema@gmail.com>
2016-12-19 12:33:35 -0600

    Added basic styles to blurb

--- client/src/components/Blurb.js
+++ client/src/components/Blurb.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableHighlight } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+
 
 const Blurb = (props) => {
   return (
     <View style={{ paddingTop: 65 }}>
-        <Text onPress={() => Actions.MapContainer()}>I am some info about {props.marker.title}</Text>
+        <Text style={styles.titleStyle}>{props.marker.title}</Text>
+        <Image 
+          style={styles.imageStyle}
+          source={{ uri: 'https://staticdelivery.nexusmods.com/mods/1151/images/1917-0-1448130696.png' }} 
+        />
+        <Text style={styles.categoryStyle}>{props.marker.category}</Text>
+        <TouchableHighlight 
+          onPress={() => Actions.MapContainer()}
+          style={styles.buttonStyle} 
+        >
+          <Text style={{ fontSize: 24 }}>Go back to map</Text>
+        </TouchableHighlight>
     </View>
   );
 };
 
+styles = {
+	titleStyle: {
+    alignSelf: 'center',
+    fontSize: 36,
+    marginBottom: 5
+	},
+  categoryStyle: {
+    alignSelf: 'center',
+    fontSize: 22
+  },
+	imageStyle: {
+    height: 300,
+    width: 300,
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderRadius: 4
+  }, 
+  buttonStyle: {
+    alignSelf: 'center',
+    backgroundColor: '#4286f4',
+    padding: 4,
+    borderRadius: 10
+  }
+};
 export default Blurb;
