68e85cf35608c7eaba2b6105684ddeab9568d404
Ethan <ethan4e@gmail.com>
2016-12-19 19:47:19 -0600

    more travis testing

--- server/__tests__/api-test.js
+++ server/__tests__/api-test.js
@@ -4,11 +4,13 @@ const server = require('../server.js');
 describe('basic server stuff', () => {
   it('returns 404 on a fake path', () => {
     return axios.get('http://localhost:4040/sahdude').catch((error) => {
+      console.log(error)
       expect(error.response.status).toBe(404);
     });
   });
-  it('returns 200 on real path', ()=>{
+  it('returns 200 on real path', () => {
     return axios.get('http://localhost:4040/').then((response) => {
+      console.log(response)
       expect(response.status).toBe(200);
     });
   });
@@ -30,7 +32,5 @@ describe('database paths', () => {
   //     expect(response.data.length).toBeTruthy();
   //   }).catch((error)=>console.log(error))
   // })
-
-  //process.exit();
 });
-  process.exitCode = 1
+  process.exitCode = 1;
