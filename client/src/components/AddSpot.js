b342ba62c1b871aef279fec1944b6769609326c7
rockinrobin714 <robindykema@gmail.com>
2016-12-20 15:06:48 -0600

    Pulling from master

--- client/src/components/AddSpot.js
+++ client/src/components/AddSpot.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 const Login = () => {
   return (
     <View style={{ paddingTop: 65 }}>
-        <Text>OMG we're totally logging in right now</Text>
+        <TextInput 
+        label='title'/
+        placeholder='Cool street art'
+        autocorrect={false}
+        />
     </View>
   );
 };
