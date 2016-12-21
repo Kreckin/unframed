b661b50a33779159146337bb433409e9a0fa869d
rockinrobin714 <robindykema@gmail.com>
2016-12-21 15:30:49 -0600

    SWEET MOTHER OF GOD THE PHOTO SELECTER WORKS

--- client/android/app/src/main/java/com/unexpectedpony/MainApplication.java
+++ client/android/app/src/main/java/com/unexpectedpony/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RCTCameraPackage(),
             new MapsPackage()
       );
