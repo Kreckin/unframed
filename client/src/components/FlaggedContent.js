fbf73e9467ec695c859aa4298a5f301951f12ae9
rockinrobin714 <robindykema@gmail.com>
2017-01-07 12:27:23 -0600

    Set up picture and button

--- client/src/components/FlaggedContent.js
+++ client/src/components/FlaggedContent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, Dimensions, TouchableOpacity, StatusBar, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
-const { width } = Dimensions.get('window');
+const { height, width } = Dimensions.get('window');
 
 class FlaggedContent extends Component {
  
@@ -12,7 +12,12 @@ class FlaggedContent extends Component {
         category: ''
     };
   }
+  onSubmit() {
+    //this is where we send out the thing to be taken down
+    Actions.MapContainer();
+  }
   render() {
+    StatusBar.setBarStyle('light-content', true);
     return (
       <View style={{ flex: 1 }}>
         <View style={styles.navBar}>
@@ -21,7 +26,10 @@ class FlaggedContent extends Component {
           </Text>
         </View>
         <View style={styles.containerStyle}>
-        
+        <Image
+          source={require('../images/monkey2.png')}
+          style={styles.imageStyle}
+        />
         
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Submit</Text>
@@ -32,21 +40,42 @@ class FlaggedContent extends Component {
   }
 }
 const styles = {
-  
-    button: {
-      backgroundColor: 'gray',
-      width: 120,
-      height: 40,
-      borderRadius: 5,
-      alignItems: 'center',
-      justifyContent: 'center',
-      margin: 10
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 18,
-      alignItems: 'center'
-    }
-  };
+  navBar: {
+    backgroundColor: '#006F60',
+    alignItems: 'center',
+    //height: 65
+  },
+  navBarText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#EFEFF4',
+    marginTop: 30,
+    marginBottom: 12
+  },
+  containerStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: '#006F60',
+    height
+  },
+  button: {
+    backgroundColor: '#00B89C',
+    width: 90,
+    borderRadius: 5,
+    margin: 10,
+    alignSelf: 'center',
+    marginBottom: 150
+  },
+  imageStyle: {
+    height: height*2/5,
+    width
+  },
+  buttonText: {
+    color: '#EFEFF4',
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center'
+  }
+};
 export default FlaggedContent;
 
