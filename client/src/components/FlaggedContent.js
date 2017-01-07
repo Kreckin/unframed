6a7bdef8a3ce09a20904c67ba6006a98344ce232
rockinrobin714 <robindykema@gmail.com>
2017-01-07 11:30:27 -0600

    Taking out modal, stripping down original design

--- client/src/components/FlaggedContent.js
+++ client/src/components/FlaggedContent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, Text, Dimensions, TextInput, TouchableOpacity } from 'react-native';
-import ModalPicker from 'react-native-modal-picker';
 import { Actions } from 'react-native-router-flux';
 
 const { width } = Dimensions.get('window');
@@ -14,64 +13,26 @@ class FlaggedContent extends Component {
     };
   }
   render() {
-      let index = 0;
-      const data = [
-
-          { key: index++, label: 'Nudity' },
-          { key: index++, label: 'Profanity' },
-          { key: index++, label: 'Copywritten' },
-          { key: index++, label: 'Offensive' },
-          { key: index++, label: 'Harmful to people or animals' },
-          { key: index++, label: 'Other' },
-      ];
     return (
-      <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>You've flagged "{this.props.spotTitle}" as innappropriate.
-        Please tell us how this post breaks the rules. </Text>
-        <ModalPicker
-          data={data}
-          onChange={(option) => { this.setState({ category: option.label })}}
-        >    
-        <TextInput
-          style={styles.inputStyle}
-          editable={false}
-          placeholder="Click to select a category"
-          value={this.state.category} 
-        />
-        </ModalPicker>
+      <View style={{ flex: 1 }}>
+        <View style={styles.navBar}>
+          <Text style={styles.navBarText}>
+          Why should "{this.props.spotTitle}" be removed?
+          </Text>
+        </View>
+        <View style={styles.containerStyle}>
+        
+        
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Submit</Text>
         </TouchableOpacity>
-        <TouchableOpacity 
-          style={styles.button}
-          onPress={() => Actions.pop()}
-        >
-          <Text style={styles.buttonText}>Back</Text>
-        </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 const styles = {
-    inputStyle: {
-      alignSelf: 'center', 
-      borderWidth: 1, 
-      borderRadius: 2,
-      borderColor: '#ccc', 
-      padding: 10, 
-      height: 40, 
-      width: width - 30
-    },
-    viewStyle: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    textStyle: {
-      textAlign: 'center',
-      fontSize: 18,
-      marginBottom: 15
-    },
+  
     button: {
       backgroundColor: 'gray',
       width: 120,
